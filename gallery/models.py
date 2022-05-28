26e46d0b59e1fa3b38cbc8131b7ee65227177c07
Blaise Hala <blaisehala@gmail.com>
2022-05-28 10:56:56 +0300

    adds images model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+  image_name = models.CharField(max_length=100)
+  image_description = models.TextField(max_length=200)
+  image_location = models.ForeignKey()
+  image_category = models.ForeignKey()
