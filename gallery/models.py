3bfac3e0ebfe5d3441997041a838848271c6519e
Blaise Hala <blaisehala@gmail.com>
2022-05-28 17:14:44 +0300

    CI:adds models

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,42 @@
 from django.db import models
 
+
+
 # Create your models here.
 
+#location 
+class Location(models.Model):
+  location_name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.location_name
+
+
+#Category
+class Category(models.Model):
+  category_name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.category_name
+
+
+
+
+#Image
 class Image(models.Model):
   image_name = models.CharField(max_length=100)
   image_description = models.TextField(max_length=200)
-  image_location = models.ForeignKey()
-  image_category = models.ForeignKey()
+  
+def __str__(self):
+  return self.image_name
+
+class Meta:
+  ordering = ['image_name']
+
+ 
+
+
+
+
   
 
