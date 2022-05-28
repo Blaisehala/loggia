a8599c428df2feda07ed01b12d1e007d3b175217
Blaise Hala <blaisehala@gmail.com>
2022-05-28 17:23:13 +0300

    Test:testing the app

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from gallery.models import Image,Location,Category
+
 
 # Create your tests here.
+
+class ImageTestCase(TestCase):
+
+  #set up method 
+   def setUp(self):
+     self.img1=Image(image_name='art', image_description='New_York')
+
+   #testing instance
+   def test_instance(self):
+     self.assertTrue(self.img1,Image)
